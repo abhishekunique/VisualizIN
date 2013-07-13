3ec92a9cf138384195e9e89b8ee0dafb381d34f9
gjhawar <gjhawar@usc.edu>
2013-07-13 03:26:43 -0700

    Facebook likes2

---
+++ likes.php
@@ -0,0 +1,32 @@
+<html>
+<head></head>
+
+<body>
+<?php
+session_start();
+$user=$_SESSION['user'];
+$likes=$_SESSION['likes'];
+
+echo $user;
+echo $likes;
+
+
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($likes); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
+</body>
+
+?>
+
+</html>
+
+
+
