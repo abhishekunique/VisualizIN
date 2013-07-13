da317e4ae6cab477d758fcc472c32ad5d8f80c41
shalin94 <shalin94@gmail.com>
2013-07-13 15:46:32 -0700

    changed multiquery

--- pages.php
+++ pages.php
@@ -54,14 +54,6 @@ if ($user) {
   </head>
   <body>
     <h1>VisualizIN</h1>
-    <?php if ($user): ?>
-    <div align="right">
-      <img width="100" height ="100" src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-      </div>
-  
-
-
-
     <?php
      echo '<iframe src="http://www.facebook.com/plugins/fan.php?connections=100&id='.$_GET['id'].'" scrolling="no" frameborder="0" style="border:none; 
 overflow:hidden; height:1000px; width:1000px;" allowTransparency="true"></iframe>'
@@ -80,7 +72,14 @@ overflow:hidden; height:1000px; width:1000px;" allowTransparency="true"></iframe
     -->
 
 
-  
+    <?php if ($user): ?>
+    <div align="right">
+      <img width="100" height ="100" src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+      </div>
+      <pre><?php // print_r($likes); ?></pre>
+    <?php else: ?>
+    <?php endif ?>
+
 
     <?php
     $like_query="select page_id,name,page_url from page where page_id in(select page_id from page_fan WHERE uid=me())";
