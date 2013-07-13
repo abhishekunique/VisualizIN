af339ed39c19b21e738d8632bb6f8f942c2312ba
shalin94 <shalin94@gmail.com>
2013-07-13 14:28:19 -0700

    changed multiquery

--- pages.php
+++ pages.php
@@ -54,9 +54,11 @@ if ($user) {
   </head>
   <body>
     <h1>VisualizIN</h1>
+    <?php if ($user): ?>
+      <img width="100" height ="100" src="https://graph.facebook.com/<?php echo $user; ?>/picture">
     <?php
      echo '<iframe src="http://www.facebook.com/plugins/fan.php?connections=100&id='.$_GET['id'].'" scrolling="no" frameborder="0" style="border:none; 
-overflow:hidden; height:600px; width:500px;" allowTransparency="true"></iframe>'
+overflow:hidden; height:800px; width:1000px;" allowTransparency="true"></iframe>'
     ?>
     <?php if ($user): ?>
       <a href="<?php echo $logoutUrl; ?>">Logout</a>
@@ -72,10 +74,8 @@ overflow:hidden; height:600px; width:500px;" allowTransparency="true"></iframe>'
     -->
 
 
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img width="100" height ="100" src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
+    
+      
       <pre><?php // print_r($likes); ?></pre>
     <?php else: ?>
     <?php endif ?>
