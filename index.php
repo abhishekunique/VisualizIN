ec846e954548b5178e42b779f8a70834286972ad
shalin94 <shalin94@gmail.com>
2013-07-13 11:07:57 -0700

    pages

--- index.php
+++ index.php
@@ -222,7 +222,7 @@ function generate_circles(namelist, xcenter, ycenter, centertext){
     httpc.send(curname);
 } */
         window.setTimeout(null,1000);
-         window.location.assign("pages.php");
+         window.location.assign("pages.php?id=232");
             
         });
         
