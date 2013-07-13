afb26c8732200b74ffef0bbf62768fa1037dace8
gjhawar <gjhawar@usc.edu>
2013-07-13 01:53:53 -0700

    Facebook login attempt

--- index.php
+++ index.php
@@ -4,12 +4,29 @@
 
 echo "abc";
 require_once("facebook.php");
-$config = array();
-$config['app_key'] = getenv('api_key');
-$config['app_secret'] = getenv('api_secret');
-$facebook = new Facebook($config);
+$facebook = new Facebook(array(
+  'appId'  => getenv('api_key');,
+  'secret' => getenv('api_secret'),
+));
 
-echo $config['app_key']." ".$config['app_secret'];
+$user = $facebook->getUser();
+
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
 
 
 ?>
