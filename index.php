200f0b455b4c793b460e0a9978b571376fbf0007
gjhawar <gjhawar@usc.edu>
2013-07-13 03:33:53 -0700

    Facebook likes2

--- index.php
+++ index.php
@@ -68,21 +68,14 @@ if ($user) {
     -->
    <?php
 
-    require_once("likes.php");
+   
     session_start();
+    require_once("likes.php");
+ 
     $_SESSION['user']=$user;
     $_SESSION['likes']=$likes; 
    ?>
 
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($likes); ?></pre>
-    <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
-    <?php endif ?>
-
+  
   </body>
 </html>
