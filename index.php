d4c069e3f8a1ca3e5535ebae3bac32d815785b0b
gjhawar <gjhawar@usc.edu>
2013-07-13 01:44:30 -0700

    Facebook secrets

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <?php
 
 echo "abc";
+require_once("facebook.php");
+$config = array();
+$config['app_key'] = getenv('api_key');
+$config['app_secret'] = getenv('api_secret');
+$facebook = new Facebook($config);
+
+echo $config['app_key']." ".$config['app_secret'];
 
 
 ?>
