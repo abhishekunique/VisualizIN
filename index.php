ab7d762ecdd7a946bd6d803c39bd4fe10780e001
shalin94 <shalin94@gmail.com>
2013-07-13 09:29:32 -0700

    YOLO

--- index.php
+++ index.php
@@ -113,9 +113,9 @@ $counts=array();
     //$fp_qurl = "https://graph.facebook.com/fql?q=".$fp_qu."&access_token=".$facebook->getAccessToken();
     //$fp_query = "SELECT uid FROM page_fan WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=me()) AND page_id=".$response[$key]['page_id'];
     $multiq = array(
-      "query1": "SELECT uid2 FROM friend WHERE uid1=me()",
-      "query2": "SELECT uid FROM page_fan WHERE uid IN (SELECT uid2 FROM #query1".")",
-      "query3": "SELECT name from user where uid in (SELECT uid FROM #query2".") AND page_id=".$response[$key]['page_id'].")",
+      "query1" => "SELECT uid2 FROM friend WHERE uid1=me()",
+      "query2" => "SELECT uid FROM page_fan WHERE uid IN (SELECT uid2 FROM #query1".")",
+      "query3" => "SELECT name from user where uid in (SELECT uid FROM #query2".") AND page_id=".$response[$key]['page_id'].")",
     );
     $friends = $facebook -> api (array(
       'method' => 'fql.multiquery',
