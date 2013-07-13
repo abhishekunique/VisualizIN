59856ea5d1d6fc37cad8bf744dfb62c2ef102ad7
shalin94 <shalin94@gmail.com>
2013-07-13 12:22:32 -0700

    YOLO

--- pages.php
+++ pages.php
@@ -267,7 +267,7 @@ function generate_circles(idlist,namelist, xcenter, ycenter, centertext){
 if(user)
 {
   getUrlVars()["name"]
-  enerate_circles(pageid,jArray,800,500, getUrlVars()["name"]);
+  generate_circles(pageid,jArray,800,500, getUrlVars()["name"]);
  }  
 function getUrlVars() {
     var vars = {};
