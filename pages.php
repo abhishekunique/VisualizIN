9531e33ea14bde9682561bbb6f00f05d7740e207
shalin94 <shalin94@gmail.com>
2013-07-13 13:21:12 -0700

    changed multiquery

--- pages.php
+++ pages.php
@@ -90,8 +90,10 @@ if ($user) {
 //$multiQuery = '{ "query1": select uid2 from friend where uid1=me()",
  //"query2":select uid from page_fan where page_id=7608631709"}';
 
+
  $multiQuery = '{ "query1": "select uid2 FROM friend WHERE uid1=me()",
- "query2":"select uid,page_id from page_fan WHERE uid in (SELECT uid2 from #query1)"}';
+ "query2":"select uid,page_id from page_fan WHERE uid in (SELECT uid2 from #query1)",
+"query3":"select name from user where uid in (select uid from #query2) and page_id='.$_GET['id'].'"}';
 
 $param123 = array(       
      'method' => 'fql.multiquery',       
