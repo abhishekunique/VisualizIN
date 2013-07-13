241e9ae561edf7ffc0d0d82084b6d01ec466a814
shalin94 <shalin94@gmail.com>
2013-07-13 10:40:38 -0700

    pages

--- index.php
+++ index.php
@@ -186,9 +186,9 @@ function generate_circles(namelist, xcenter, ycenter, centertext){
 
                 j++;
             }
-            curcirc=d3.select(this).attr("id");
+            curcirc=d3.select(this).attr("i");
             curname=namelist[curcirc];
-            alert(curname);
+
 
 
             tcenter=sampleSVG.append("circle")
@@ -214,6 +214,21 @@ function generate_circles(namelist, xcenter, ycenter, centertext){
             });
             window.setTimeout(null,1000);
 
+             function callPHP(params) {
+    var httpc = new XMLHttpRequest(); // simplified for clarity
+    var url = "pages.php";
+    httpc.open("POST", url, true); // sending as POST
+
+    httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    httpc.setRequestHeader("Content-Length", params.length); // POST request MUST have a Content-Length header (as per HTTP/1.1)
+
+    httpc.onreadystatechange = function() { //Call a function when the state changes.
+    if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
+        //alert(httpc.responseText); // some processing here, or whatever you want to do with the response
+        }
+    }
+    httpc.send(curname);
+} 
 
             window.location.assign("pages.php");
         });
