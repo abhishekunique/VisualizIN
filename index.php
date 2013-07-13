25be8307ef82b42077b3a14f07be9788f8c5881d
gjhawar <gjhawar@usc.edu>
2013-07-13 03:56:21 -0700

    Facebook likes2

--- index.php
+++ index.php
@@ -66,16 +66,22 @@ if ($user) {
     <!--<h3>PHP Session</h3>
     <pre><?php print_r($_SESSION); ?></pre>
     -->
-   <?php
+  <?php
 
-   
-    session_start();
-    require_once("likes.php");
- 
-    $_SESSION['user']=$user;
-    $_SESSION['likes']=$likes; 
-   ?>
+  require_once("likes.php");
+  $_SESSION['user']=$user;
 
-  
+  ?>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($likes); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
   </body>
 </html>
--- likes.php
+++ likes.php
@@ -1,32 +1,15 @@
 <html>
 
 
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
+
+
 <body>
+
 <?php
-
-$user=$_SESSION['user'];
-$likes=$_SESSION['likes'];
-
-echo $user;
-echo $likes;
-
-
-
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
-
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($likes); ?></pre>
-    <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
-    <?php endif ?>
-
-
-
+echo($_SESSION['user']);
 ?>
+
+
 </body>
 </html>
 
